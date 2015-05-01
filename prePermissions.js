e5ff511c64639af86231954aae1164d52d15ba59
Emily <emily@hyperlayer.io>
2015-05-01 15:51:59 -0700

    Chrome-specific alert added

--- prePermissions.js
+++ prePermissions.js
@@ -1,7 +1,17 @@
 
+function printUserAgent () {
+    console.log(navigator.userAgent);
+}
+
 
 function getGeoLocation() {
-            alert("You are about to be asked to share your location.");
-            navigator.geolocation.getCurrentPosition( function() {} );
-        }
+    if (navigator.userAgent.indexOf('Chrome') !== -1 ) {
+        // User is using Chrome
+        alert("Click on the icon to the right of the URL and enable location services."); // Change alert message to be clearer to user; may need to check the way Chrome behaves on different platforms
+    } else {
+        alert("You are about to be asked to share your location.");
+        navigator.geolocation.getCurrentPosition( function() {} );
+    }
+}
+
         
