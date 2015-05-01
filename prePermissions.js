38cf06ea2a1a93cb366754112472db58bfd835bc
Emily <emily@hyperlayer.io>
2015-05-01 15:19:24 -0700

    Separate JS file added

---
+++ prePermissions.js
@@ -0,0 +1,7 @@
+
+
+function getGeoLocation() {
+            alert("You are about to be asked to share your location.");
+            navigator.geolocation.getCurrentPosition( function() {} );
+        }
+        
