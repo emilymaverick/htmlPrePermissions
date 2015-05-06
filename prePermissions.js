0f948ff869f8bab57ff5ef040ab89ffcf69b106b
Emily <emily@hyperlayer.io>
2015-05-06 16:32:34 -0700

    Added demo for how to print long and lat to page

--- prePermissions.js
+++ prePermissions.js
@@ -4,14 +4,20 @@ function printUserAgent () {
 }
 
 
-function getGeoLocation() {
+
+function getGeoLocation( callback ) {
     if (navigator.userAgent.indexOf('Chrome') !== -1 ) {
         // User is using Chrome
         alert("Click on the icon to the right of the URL and enable location services."); // Change alert message to be clearer to user; may need to check the way Chrome behaves on different platforms
     } else {
         alert("You are about to be asked to share your location.");
-        navigator.geolocation.getCurrentPosition( function() {} );
+        
     }
+    navigator.geolocation.getCurrentPosition( callback );
 }
+
+
+        
+        
 
         
