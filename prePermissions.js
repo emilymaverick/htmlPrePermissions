7c1093cfeb454dc3918d13219e6aec81752e4ae3
Emily <emily@hyperlayer.io>
2015-05-13 16:56:44 -0700

    Added customization to alert message and added alertMessage as an optional string for getGeolocation

--- prePermissions.js
+++ prePermissions.js
@@ -5,12 +5,25 @@ function printUserAgent () {
 
 
 
-function getGeoLocation( callback ) {
+function getGeoLocation( callback, alertMessage ) {
+// This function first checks the user's browser vendor. If using Chrome, will instruct user to manage their own location settings
+// in order to share their location; if using any other browser vendor, will trigger an alert warning the user that they are about
+// to be asked to share their location.
+//
+// This function returns nothing.
+//
+// callback - function to be called when the user location is available
+// alertMessage - optional string that can be customized based on what you want to say in the pre-permissions dialogue box, e.g.
+//    "You are about to be asked to share your location." 
+
+    if ( alertMessage === undefined) {
+        alertMessage = "You are about to be asked to share your location."
+    }
     if (navigator.userAgent.indexOf('Chrome') !== -1 ) {
         // User is using Chrome
-        alert("Click on the icon to the right of the URL and enable location services."); // Change alert message to be clearer to user; may need to check the way Chrome behaves on different platforms
+        alert( alertMessage + "  Click on the icon to the right of the URL and enable location services."); // Change alert message to be clearer to user; may need to check the way Chrome behaves on different platforms
     } else {
-        alert("You are about to be asked to share your location.");
+        alert( alertMessage ); 
         
     }
     navigator.geolocation.getCurrentPosition( callback );
